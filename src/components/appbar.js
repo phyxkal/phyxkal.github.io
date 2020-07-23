00b49710015c20be71eebe37c2b9a821b0f50ae1
phyxkal <haikalvidya@gmail.com>
2020-07-23 09:34:04 +0700

    add some fucking change about routing

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -9,7 +9,7 @@ export default function Appbar() {
           <Intent to="/">Home</Intent>
         </li>
         <li className="nav-item mx-4">
-          <Intent to="/projects">Projects</Intent>
+          <Intent to="./projects">Projects</Intent>
         </li>
       </ul>
     </div>
