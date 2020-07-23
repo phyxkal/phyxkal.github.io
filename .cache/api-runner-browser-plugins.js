e5f2420ba363ebc5d47a38b6b88ad1497c6bed01
phyxkal <haikalvidya@gmail.com>
2020-07-23 21:41:21 +0700

    delete everything

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-transition-link/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
